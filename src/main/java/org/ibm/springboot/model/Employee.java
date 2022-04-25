230f27c3f9923bf9b38fc6ec3d1a0b59bc40c518
subbareddygithub <Annapureddy@123>
2022-04-25 17:23:45 +0530

    initial commit

---
+++ src/main/java/org/ibm/springboot/model/Employee.java
@@ -0,0 +1,59 @@
+package org.ibm.springboot.model;
+
+public class Employee {
+	
+	private String _rev;
+	private String _id;
+	
+	private String firstName;
+
+	private String lastName;
+	
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String get_rev() {
+		return _rev;
+	}
+
+	public void set_rev(String _rev) {
+		this._rev = _rev;
+	}
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	
+}
